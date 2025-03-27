c51a1f64b5741c2f98c06f0a21a9366a771a3fbd
aren209 <aren209@yandex.ru>
2025-03-27 10:18:06 +0300

    lab2 episode_1 bad images

---
+++ lab_2_np/np_episode_1/np_episode_2.py
@@ -0,0 +1,27 @@
+import numpy as np
+from PIL import Image
+
+for j in range(3):
+    img = Image.open(f'lunar_images/lunar0{j+1}_raw.jpg')
+    data = np.array(img)
+
+    a = data.min()
+    b = data.max()
+    
+    k = 255 / (b - a)
+    p = (255 - k * (a + b)) / 2
+    
+    rows = data.shape[0]
+    columns = data.shape[1]
+    
+    updated_data = np.empty_like(data)
+    
+    # print(rows)
+    # print(columns)
+    for u in range(rows):
+        for i in range(columns):
+            x = data[u][i]
+            updated_data[u][i] = k * x + p    
+
+    res_img = Image.fromarray(updated_data)
+    res_img.save(f'result_images/updated_lunar{j+1}.jpg')
